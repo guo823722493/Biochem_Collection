bf459e58f24edee9bd0171b306a3fa2c8ad1f8a9
guo823722493 <823722493@qq.com>
2017-06-02 15:26:56 +0800

    video autofit

--- app/src/main/java/com/biochemical/biochemicalcollections/Chapter_1.java
+++ app/src/main/java/com/biochemical/biochemicalcollections/Chapter_1.java
@@ -1,28 +1,53 @@
 package com.biochemical.biochemicalcollections;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 
 public class Chapter_1 extends AppCompatActivity {
+    WebView page_1;
+    ImageButton back;
+    ImageButton setting;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chapter_1_layout);
-        ImageButton back = (ImageButton) findViewById(R.id.back);
-        ImageButton setting = (ImageButton) findViewById(R.id.setting);
-        WebView page_1 = (WebView) findViewById(R.id.Page_1);
+        back = (ImageButton) findViewById(R.id.back);
+        setting = (ImageButton) findViewById(R.id.setting);
+        page_1 = (WebView) findViewById(R.id.Page_1);
+
         WebSettings mwebsettings=page_1.getSettings();
         mwebsettings.setJavaScriptEnabled(true);
+        mwebsettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        mwebsettings.setPluginState(WebSettings.PluginState.ON);
+        mwebsettings.setAllowFileAccess(true);
+        mwebsettings.setLoadWithOverviewMode(true);
+        mwebsettings.setUseWideViewPort(true);
+        page_1.setVisibility(View.VISIBLE);
+        mwebsettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+       mwebsettings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        mwebsettings.setSupportMultipleWindows(true);
+        mwebsettings.setBuiltInZoomControls(true);
+        mwebsettings.setDomStorageEnabled(true);
+        page_1.setLayerType(View.LAYER_TYPE_HARDWARE,null);
+        if(Build.VERSION.SDK_INT>=21)
+        {
+            mwebsettings.setMixedContentMode(mwebsettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
         final ProgressBar mProgressBar=(ProgressBar)findViewById(R.id.mProgressBar);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,7 +62,7 @@ public class Chapter_1 extends AppCompatActivity {
                 startActivity(set);
             }
         });
-        page_1.loadUrl("https://www.baidu.com/");
+        page_1.loadUrl("file:///android_asset/index.html");
         page_1.setWebViewClient(new WebViewClient() {
 
             @Override
@@ -62,6 +87,19 @@ public class Chapter_1 extends AppCompatActivity {
                 }
             }
         });
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        page_1.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        page_1.onResume();
     }
 }
 
